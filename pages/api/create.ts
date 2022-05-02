89f15fdfaf8ceef2ebfa33b618ec83919d865957
pat <patriciarosalis@live.com.pt>
2022-05-02 22:35:11 +0100

    first commit

---
+++ pages/api/create.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
+
+type Data = {
+    name: string;
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    try {
+        const { user } = req.body;
+        // const user: Prisma.UserCreateInput = JSON.parse(req.body);
+        const savedUser = await prisma.user.create({
+            data: user,
+        });
+        res.status(200).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ message: 'Something went wrong' });
+    }
+}
